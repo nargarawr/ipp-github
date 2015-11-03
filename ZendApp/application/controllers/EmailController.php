d85192ee20aa9d1ad8c0b92629f5facb3e106bce
Craig Knott <cxk01u@googlemail.com>
2015-11-02 20:03:53 -0500

    Ability to send emails

---
+++ ZendApp/application/controllers/EmailController.php
@@ -0,0 +1,56 @@
+<?
+
+class EmailController extends BaseController {
+
+    public function init() {
+        parent::init();
+        $this->view->isExternal = false;
+    }
+
+    public function emailAction() {
+        $this->_helper->layout()->disableLayout();
+        $this->_helper->viewRenderer->setNoRender(true);
+
+        $response = $this->sendEmail(
+            'cxk01u@gmail.com', 
+            'Confirm email address', 
+            'Please confirm your email (although this text will come fromthe view hopefully)'
+        );
+        print_r($response);
+    }
+
+    public function confirmemailAction() {}
+    public function forgotpasswordAction() {}
+    public function sendannouncementAction() {}
+
+    public function sendEmail($to, $subject, $body) {
+        $url = 'https://api.sendgrid.com/';
+        $user = 'nicewayto';
+        $pass = '12QWASzx';
+
+        $params = array(
+            'api_user'  => $user,
+            'api_key'   => $pass,
+            'to'        => $to,
+            'subject'   => $subject,
+            'html'      => $body,
+            'text'      => $body,
+            'from'      => 'noreply@niceway.to',
+        );
+
+        $request =  $url.'api/mail.send.json';
+
+        $session = curl_init($request);
+        curl_setopt ($session, CURLOPT_POST, true);
+        curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($session, CURLOPT_HEADER, false);
+        curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+        curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($session);
+        curl_close($session);
+
+        return $response;
+    }
+
+}
