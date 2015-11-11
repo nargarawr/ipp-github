964f4dda8c94f1ced1eed3358f8b82984ae2334d
Craig Knott <cxk01u@googlemail.com>
2015-11-11 20:50:06 +0000

    Added some files

---
+++ ZendApp/public/js/global[Conflict].js
@@ -0,0 +1,36 @@
+var navBar = $('.navbar-right').find('.dropdown-toggle');
+var navName = $('#nav-username').val();
+var caret = '<span class="caret"></span>';
+
+$(window).resize(function () {
+    if (window.innerWidth < 1066) {
+        if ($(navBar).html() != navName + caret) {
+            $(navBar).html(navName + caret);
+        }
+    } else {
+        if ($(navBar).html() != 'Logged in as ' + navName + caret) {
+            $(navBar).html('Logged in as ' + navName + caret)
+        }
+    }
+});
+
+
+/*
+
+ to send an email through AJAX
+ */
+function sendEmail() {
+    $.ajax({
+        type:    'POST',
+        url:     '/email',
+        data:    {
+            templateName: 'confirmemail',
+            name:         'RALPH',
+            to:           ['cxk01u@gmail.com'],
+            subject:      'Please consider confirming your email'
+        },
+        success: function (response) {
+            console.log(response);
+        }
+    });
+}
---
+++ ZendApp/public/js/user/routes[Conflict].js
@@ -0,0 +1,72 @@
+var tm;
+
+$(document).ready(function () {
+    $('.delBtn').click(function (e) {
+        var url = this.href;
+        e.preventDefault();
+        $.confirm({
+            title:           'Delete point?',
+            icon:            'fa fa-warning',
+            content:         'Are you sure you wish to delete this route? This action is irreversible.',
+            theme:           'black',
+            confirmButton:   'Delete',
+            keyboardEnabled: true,
+            confirm:         function () {
+                window.location = url;
+            }
+        });
+    });
+
+    tm = new TableManager($('#routeTable'));
+});
+
+
+var TableManager = Class.extend({
+    init:    function (table) {
+        this.table = table;
+        this.rows = this.getRows();
+
+    },
+    getRows: function () {
+        var rowClasses = this.table.find($('.routeRow'));
+        var rows = [];
+        for (var i = 0; i < rowClasses.length; i += 2) {
+            rows.push(new TableRow(rowClasses[i], rowClasses[i + 1]));
+        }
+        return rows;
+    }
+});
+
+var TableRow = Class.extend({
+    init:           function (row, pointsRow) {
+        this.row = $(row);
+        this.pointsRow = $(pointsRow);
+
+        this.caretDown = this.row.find('.fa-chevron-circle-down');
+        this.caretUp = this.row.find('.fa-chevron-circle-up');
+
+        this.setupListeners();
+    },
+    setupListeners: function () {
+        var _self = this;
+
+        this.caretUp.click(function () {
+            _self.pointsRow.addClass("hidden");
+            _self.caretUp.addClass("hidden");
+            _self.caretDown.removeClass("hidden");
+            _self.pointsRow.find("td").removeClass("noBorder");
+            _self.row.find("td").removeClass("noBottomPad");
+        });
+
+        this.caretDown.click(function () {
+            _self.pointsRow.removeClass("hidden");
+            _self.caretDown.addClass("hidden");
+            _self.caretUp.removeClass("hidden");
+            _self.pointsRow.find("td").addClass("noBorder");
+            _self.row.find("td").addClass("noBottomPad");
+        });
+    }
+
+});
+
+
